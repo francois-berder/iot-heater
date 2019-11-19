1e567104698028bce274342b7b1e400dea86236c
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-19 21:47:25 +0000

    Start Wifi AP in uncommissioned state
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/uncomissioned.cpp
+++ heater/uncomissioned.cpp
@@ -2,6 +2,7 @@
 #include "uncommissioned.h"
 #include "Arduino.h"
 #include "Ticker.h"
+#include <ESP8266WiFi.h>
 
 #define SLOW_BLINK_PERIOD     (500) /* In ms */
 
@@ -18,6 +19,13 @@ void setup_uncommissioned(void)
     pinMode(LED1_PIN, OUTPUT);
     pinMode(LED2_PIN, OUTPUT);
     leds_ticker.attach_ms(SLOW_BLINK_PERIOD, toggle_leds);
+
+    /* Create Wifi AP */
+    char ssid[16];
+    sprintf(ssid, "heater-%08X", ESP.getChipId());
+    char password[16];
+    sprintf(password, "%08X", ESP.getChipId());
+    WiFi.softAP(ssid, password);
 }
 
 void loop_uncommissioned(void)
