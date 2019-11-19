423b6f600150533ec0e09d08dbab1a8ca890e018
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-19 21:40:50 +0000

    Blink LEDs in uncommissioned state
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/uncomissioned.cpp
+++ heater/uncomissioned.cpp
@@ -1,8 +1,23 @@
+#include "board.h"
 #include "uncommissioned.h"
+#include "Arduino.h"
+#include "Ticker.h"
+
+#define SLOW_BLINK_PERIOD     (500) /* In ms */
+
+static Ticker leds_ticker;
+
+static void toggle_leds()
+{
+    digitalWrite(LED1_PIN, !digitalRead(LED1_PIN));
+    digitalWrite(LED2_PIN, !digitalRead(LED2_PIN));
+}
 
 void setup_uncommissioned(void)
 {
-    /* @todo Not yet implemented */
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+    leds_ticker.attach_ms(SLOW_BLINK_PERIOD, toggle_leds);
 }
 
 void loop_uncommissioned(void)
