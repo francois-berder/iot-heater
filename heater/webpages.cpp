8b9513d1848534168fca7ff8363d7f4c74db630c
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-20 20:56:41 +0000

    Add registration form for uncommissioned webpage
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/webpages.cpp
+++ heater/webpages.cpp
@@ -5,10 +5,22 @@ const char uncommissioned_index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <h2>Heater</h2>
+    <h2>Heater registration</h2>
+    <form action="/register" method="post">
+        Name:<br>
+        <input type="text" name="name" maxlength="63">
+        <br>
+        Wifi name:<br>
+        <input type="text" name="ssid" maxlength="63">
+        <br>
+        Wifi password:<br>
+        <input type="password" name="password" maxlength="63">
+        <br>
+        <input type="submit" value="Register">
+    </form>
 </body>
 </html>
 )rawliteral";
