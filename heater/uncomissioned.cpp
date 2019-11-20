736e27efd4eac978ba941594e597377e3f27bf59
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-20 20:57:03 +0000

    Use MAC address to generate SSID and password
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/uncomissioned.cpp
+++ heater/uncomissioned.cpp
@@ -29,10 +29,14 @@ void setup_uncommissioned(void)
     leds_ticker.attach_ms(SLOW_BLINK_PERIOD, toggle_leds);
 
     /* Create Wifi AP */
-    char ssid[16];
-    sprintf(ssid, "heater-%08X", ESP.getChipId());
-    char password[16];
-    sprintf(password, "%08X", ESP.getChipId());
+    byte mac[6];
+    WiFi.macAddress(mac);
+    char ssid[32];
+    sprintf(ssid, "heater-%02X%02X%02X%02X%02X%02X",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    char password[32];
+    sprintf(password, "%02X%02X%02X%02X%02X%02X",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     IPAddress apIP(192, 168, 1, 1);
     WiFi.mode(WIFI_AP);
     WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
