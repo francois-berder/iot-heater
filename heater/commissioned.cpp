02d3b3735ad89e60994e34702a4ad5e020a2dc9a
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-20 09:54:29 +0000

    Setup LEDs and button in commissioned state
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/commissioned.cpp
+++ heater/commissioned.cpp
@@ -1,8 +1,22 @@
+#include "board.h"
 #include "commissioned.h"
+#include "Arduino.h"
+
+static void button_isr()
+{
+    /* @todo Not yet implemented */
+}
 
 void setup_commissioned()
 {
-    /* @todo Not yet implemented */
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+
+    digitalWrite(LED1_PIN, 1);
+    digitalWrite(LED2_PIN, 0);
+
+    pinMode(BUTTON_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_isr, CHANGE);
 }
 
 void loop_commissioned()
