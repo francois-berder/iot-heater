d7b6ebd9b29368780c6e0803ddfda0a083519525
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-12-15 15:26:12 +0000

    Fix NodeMCU pins
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater/board.h
+++ heater/board.h
@@ -3,9 +3,9 @@
 
 #define BUTTON_PIN      (12)
 #define LED1_PIN        (13)
-#define LED2_PIN        (15)
+#define LED2_PIN        (14)
 
-#define LEFT_OUTPUT_PIN     (0)
-#define RIGHT_OUTPUT_PIN    (2)
+#define LEFT_OUTPUT_PIN     (4)
+#define RIGHT_OUTPUT_PIN    (5)
 
 #endif
