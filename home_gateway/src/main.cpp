e81e4b1fd38bf682905fe0432ea468d956078d3d
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2020-02-11 22:08:04 +0000

    home_gateway: Implement argument parsing
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- home_gateway/src/main.cpp
+++ home_gateway/src/main.cpp
@@ -5,18 +5,54 @@
 #include "device_server.hpp"
 #include "sms_server.hpp"
 
+#define DEFAULT_DEVICE_SERVER_PORT     (32322)
+
+static void print_help(char *program_name)
+{
+    std::cout << "Usage: " << program_name << " [options]\n"
+              << "Options: \n"
+              << "    --device-server-port <port>       Set device server port\n"
+              << "    --help, -h                        Print help\n"
+              << std::flush;
+}
+
 int main(int argc, char **argv)
 {
-    (void)argc;
+    char *program_name = argv[0];
+    int device_server_port = DEFAULT_DEVICE_SERVER_PORT;
+
+    argc--;
+    argv++;
+    while (argc) {
+        std::string opt(argv[0]);
+        if (opt == "--device-server-port" && argc >= 2) {
+            std::string optarg(argv[1]);
+            device_server_port = std::stoi(optarg);
+            argc--;
+            argv++;
+        } else if (opt == "--help" || opt == "-h") {
+            print_help(program_name);
+            return 0;
+        } else {
+            std::cerr << "Invalid option: \"" << opt << '\"' << std::endl;
+            print_help(program_name);
+            return -1;
+        }
+
+        argc--;
+        argv++;
+    }
+
+
     Logger::instance().startLogging(".");
     {
         std::stringstream ss;
-        ss << argv[0] << " started";
+        ss << program_name << " started";
         Logger::info(ss.str());
     }
 
     HomeGateway device_manager;
-    DeviceServer device_server(32322,
+    DeviceServer device_server(device_server_port,
                                std::bind(&HomeGateway::handleNewDevice, &device_manager, std::placeholders::_1));
     device_server.start();
 
