76db930cfdc169d931f51ceeb5cf89701e1f21a4
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2020-02-11 21:59:48 +0000

    home_gateway: Implement main
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- home_gateway/src/main.cpp
+++ home_gateway/src/main.cpp
@@ -1,10 +1,34 @@
-#include <iostream>
+#include <functional>
+#include <sstream>
+#include "home_gateway.hpp"
+#include "logger.hpp"
+#include "device_server.hpp"
+#include "sms_server.hpp"
 
 int main(int argc, char **argv)
 {
     (void)argc;
-    (void)argv;
-    std::cout << argv[0] << " starting..." << std::endl;
+    Logger::instance().startLogging(".");
+    {
+        std::stringstream ss;
+        ss << argv[0] << " started";
+        Logger::info(ss.str());
+    }
+
+    HomeGateway device_manager;
+    DeviceServer device_server(32322,
+                               std::bind(&HomeGateway::handleNewDevice, &device_manager, std::placeholders::_1));
+    device_server.start();
+
+    SMSServer sms_server(std::bind(&HomeGateway::handleSMSCommand, &device_manager, std::placeholders::_1, std::placeholders::_2));
+    sms_server.start();
+    while (true) {
+        device_manager.process();
+    }
+
+    sms_server.stop();
+    device_server.stop();
+    Logger::instance().stopLogging();
 
     return 0;
 }
