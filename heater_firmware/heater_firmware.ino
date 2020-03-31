8045dcbb91e1dd5e834c6f594017a40e966cf953
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2020-03-31 11:48:44 +0100

    heater_firmware: Print git hash and build time during initialization
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater_firmware/heater_firmware.ino
+++ heater_firmware/heater_firmware.ino
@@ -2,11 +2,23 @@
 #include "settings.h"
 #include "uncommissioned.h"
 
+#define xstr(s) str(s)
+#define str(s) #s
+
+#ifndef GIT_HASH
+#define GIT_HASH    "unknown"
+#endif
+
+#ifndef BUILD_TIME
+#define BUILD_TIME  "unknown"
+#endif
+
 bool commissioned;
 
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("\nHeater controller - " xstr(GIT_HASH) " - " xstr(BUILD_TIME));
 
     commissioned = settings_load();
 
