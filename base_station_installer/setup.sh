35734deb1cb267cf158af6de0a7b3e6369793629
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2020-04-12 21:38:04 +0100

    base_station_installer: Ensure dig is available in the shell
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- base_station_installer/setup.sh
+++ base_station_installer/setup.sh
@@ -20,7 +20,7 @@ sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd
 # Upgrade packages and install dependencies
 apt update
 apt -y upgrade
-apt -y install unattended-upgrades smstools git build-essential i2c-tools python-rpi.gpio python-dev python-serial python-smbus python-jinja2 python-xmltodict python-psutil python-pip
+apt -y install unattended-upgrades smstools git build-essential i2c-tools python-rpi.gpio python-dev python-serial python-smbus python-jinja2 python-xmltodict python-psutil python-pip dnsutils
 
 # Disable HDMI
 echo "Disabling HDMI"
