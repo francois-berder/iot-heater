73e42a85923fa200aa3322e52cbec8da9ba95026
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2020-04-24 22:49:10 +0100

    heater_firmware: Fix all I/O pinout
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- heater_firmware/board.h
+++ heater_firmware/board.h
@@ -1,11 +1,11 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#define BUTTON_PIN      (12)
-#define LED1_PIN        (13)
-#define LED2_PIN        (14)
+#define BUTTON_PIN      (5)
+#define LED1_PIN        (0)
+#define LED2_PIN        (4)
 
-#define POSITIVE_OUTPUT_PIN     (4)
-#define NEGATIVE_OUTPUT_PIN     (5)
+#define POSITIVE_OUTPUT_PIN     (12)
+#define NEGATIVE_OUTPUT_PIN     (13)
 
 #endif
