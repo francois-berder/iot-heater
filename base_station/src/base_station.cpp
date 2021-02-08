61cf5c1f09a125d62946e9383d5957008d124c09
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2021-02-08 09:07:06 +0000

    base_station: Fix timestamp format
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- base_station/src/base_station.cpp
+++ base_station/src/base_station.cpp
@@ -544,7 +544,7 @@ std::string BaseStation::buildWebpage()
         {
             time_t ts = h.getLastRequestTimestamp();
             char buf[128];
-            strftime(buf, sizeof(buf) - 1, "%d-%m-%Y %H:%M:%S", localtime(&ts));
+            strftime(buf, sizeof(buf) - 1, "%Y-%m-%d %H:%M:%S", localtime(&ts));
             ss << "<td>" << buf << "</td>";
         }
 
